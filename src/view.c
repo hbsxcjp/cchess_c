e1223b546244912e268db2aed97c75ce2fbda785
hbsxcjp <hbsxcjp@163.com>
2020-01-01 00:16:13 +0800

    U260 commit

--- src/view.c
+++ src/view.c
@@ -7,14 +7,14 @@
 //#include <termios.h>
 
 // _WIN32 _LINUX
+static const wchar_t* BLANKSTR = L"－－";
 
 static void __displayFileList(wchar_t* fileNames[], int first, int last, Pos pos)
 {
-    wchar_t* BLANKSTR = L"－－";
     wchar_t wstr[THOUSAND_SIZE], pageWstr[THOUSAND_SIZE * 8];
-    swprintf(pageWstr, FILENAME_MAX, L"%s >> 第%3d～%3d个文件：\n\n", fileNames[0], first, last);
+    swprintf(pageWstr, FILENAME_MAX, L"%s >> 第%d～%d个文件：\n\n", fileNames[0], first, last);
     for (int i = first; i <= last; ++i) {
-        swprintf(wstr, 102, L"%3d. %s\n", i, fileNames[i + 1]);
+        swprintf(wstr, 102, L"%3d. %s\n", i, fileNames[i]);
         wcscat(pageWstr, wstr);
     }
     swprintf(wstr, THOUSAND_SIZE, L"\n\n操作>>  b/p:%s  空格/n:%s  q:退出\n"
@@ -30,7 +30,7 @@ static int getFileIndex(wchar_t* fileNames[], int fileCount)
 {
     int key = 0, pageIndex = 0, perPageCount = 20,
         pageCount = (fileCount - 1) / perPageCount + 1,
-        first = 0, last = fileCount < perPageCount ? fileCount : perPageCount;
+        first = 1, last = fileCount < perPageCount ? fileCount : perPageCount;
     Pos pos = FIRST;
     if (fileCount <= perPageCount)
         pos &= END;
@@ -40,7 +40,7 @@ static int getFileIndex(wchar_t* fileNames[], int fileCount)
         case ' ':
         case 'n':
             if (pageIndex < pageCount - 1) {
-                first = ++pageIndex * perPageCount;
+                first = ++pageIndex * perPageCount + 1;
                 last = first + perPageCount;
                 pos = MIDDLE;
                 if (last >= fileCount) {
@@ -53,7 +53,7 @@ static int getFileIndex(wchar_t* fileNames[], int fileCount)
         case 'b':
         case 'p':
             if (pageIndex > 0) {
-                first = --pageIndex * perPageCount;
+                first = --pageIndex * perPageCount + 1;
                 last = first + perPageCount;
                 pos = MIDDLE;
                 if (pageIndex == 0)
@@ -81,17 +81,16 @@ static int getFileIndex(wchar_t* fileNames[], int fileCount)
 
 static void __displayInstance(const Instance* ins, const wchar_t* fileName)
 {
-    wchar_t* BLANKSTR = L"－";
     wchar_t wstr[THOUSAND_SIZE * 2], pageWstr[THOUSAND_SIZE * 8];
     swprintf(pageWstr, FILENAME_MAX, L"%s(着数:%d 注数:%d 着深:%d 变深:%d):\n\n",
         fileName, ins->movCount_, ins->remCount_, ins->maxRow_, ins->maxCol_);
     getBoardString(wstr, ins->board);
     wcscat(pageWstr, wstr);
     swprintf(wstr, THOUSAND_SIZE, L"\n操作>>  b/p:%s  空格/n:%s  g/o:%s  q:退出\n"
-                                  L"\n着法>> (%2d,%2d) %c=>%s\n       注解:%s\n",
-        hasPre(ins) ? L"前" : BLANKSTR,
-        hasNext(ins) ? L"后" : BLANKSTR,
-        hasOther(ins) ? L"变" : BLANKSTR,
+                                  L"\n着法>> (%2d,%2d) %c=>%s  注解:%s\n",
+        hasPre(ins) ? L"前着" : BLANKSTR,
+        hasNext(ins) ? L"后着" : BLANKSTR,
+        hasOther(ins) ? L"变着" : BLANKSTR,
         ins->currentMove->nextNo_,
         ins->currentMove->otherNo_,
         (isStart(ins) ? L'　' : (getColor_zh(ins->currentMove->zhStr) == RED ? L'红' : L'黑')),
