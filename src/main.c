e649100e20668e5d1711dcdcc59c42e6330ba219
hbsxcjp <hbsxcjp@163.com>
2020-05-27 08:38:44 +0800

    push commit

--- src/main.c
+++ src/main.c
@@ -1,71 +1,9 @@
 //#define NDEBUG
-
-#include "head/base.h"
-#include "head/board.h"
-#include "head/chessManual.h"
-//#include "head/console.h"
-//#include "head/move.h"
-#include "CUnit/CUnit.h"
-#include "CUnit/Basic.h"
-#include "head/md5.h"
-#include "head/piece.h"
-#include "head/sha1.h"
-#include "head/tools.h"
-#include <time.h>
+#include "head/unitTest.h"
 
 int main(int argc, char const* argv[])
 {
-    setlocale(LC_ALL, "");
-    setbuf(stdin, NULL);
-
-    time_t time0;
-    time(&time0);
-
-    //FILE* fout = stdout;
-    FILE* fout = fopen("s", "w");
-    if (!fout)
-        return -1;
-    //fwprintf(fout, L"输出中文成功了！\n");
-
-    testPiece(fout);
-    testBoard(fout);
-    testChessManual(fout);
-
-    const char* chessManualDirName[] = {
-        "chessManual/示例文件",
-        "chessManual/象棋杀着大全",
-        "chessManual/疑难文件",
-        "chessManual/中国象棋棋谱大全"
-    };
-    int size = sizeof(chessManualDirName) / sizeof(chessManualDirName[0]);
-    //testTools(fout, chessManualDirName, size, ".xqf");
-    //testsha1();
+    unitTest(argc, argv);
     
-    //*
-    if (argc == 4)
-        testTransDir(chessManualDirName, size, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
-    else if (argc == 2) {
-        testTransDir(chessManualDirName, size, atoi(argv[1]), 1, 2);
-        //testTransDir(chessManualDirName, size, 2, 6, 6);
-    }
-    //*/
-
-    //doView();
-    //testConview();
-
-    //PConsole pconsole = newConsole("01.xqf");
-    //delConsole(pconsole);
-
-    //wchar_t* dir = L"C:\\棋谱\\示例文件.xqf";
-    //wchar_t* dir = L"C:\\棋谱\\象棋杀着大全.xqf";
-    //textView(dir);
-
-    time_t time1;
-    time(&time1);
-    double t = difftime(time1, time0);
-    //printf("  use:%6.2fs\n", t);
-    printf("\nuse:%6.3fs\n", t); // , ctime(&time0), ctime(&time1)
-
-    fclose(fout);
     return 0;
 }
---
+++ src/unitTest.c
@@ -0,0 +1,58 @@
+#include "head/unitTest.h"
+
+int unitTest(int argc, char const* argv[])
+{
+    setlocale(LC_ALL, "");
+    setbuf(stdin, NULL);
+
+    time_t time0;
+    time(&time0);
+
+    //FILE* fout = stdout;
+    FILE* fout = fopen("s", "w");
+    if (!fout)
+        return -1;
+    //fwprintf(fout, L"输出中文成功了！\n");
+
+    testPiece(fout);
+    testBoard(fout);
+    testChessManual(fout);
+
+    const char* chessManualDirName[] = {
+        "chessManual/示例文件",
+        "chessManual/象棋杀着大全",
+        "chessManual/疑难文件",
+        "chessManual/中国象棋棋谱大全"
+    };
+    int size = sizeof(chessManualDirName) / sizeof(chessManualDirName[0]);
+    //testTools(fout, chessManualDirName, size, ".xqf");
+    //testsha1();
+
+    //*
+    if (argc == 4)
+        testTransDir(chessManualDirName, size, atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+    else if (argc == 2) {
+        testTransDir(chessManualDirName, size, atoi(argv[1]), 1, 2);
+        //testTransDir(chessManualDirName, size, 2, 6, 6);
+    }
+    //*/
+
+    //doView();
+    //testConview();
+
+    //PConsole pconsole = newConsole("01.xqf");
+    //delConsole(pconsole);
+
+    //wchar_t* dir = L"C:\\棋谱\\示例文件.xqf";
+    //wchar_t* dir = L"C:\\棋谱\\象棋杀着大全.xqf";
+    //textView(dir);
+
+    time_t time1;
+    time(&time1);
+    double t = difftime(time1, time0);
+    //printf("  use:%6.2fs\n", t);
+    printf("\nuse:%6.3fs\n", t); // , ctime(&time0), ctime(&time1)
+
+    fclose(fout);
+    return 0;
+}
